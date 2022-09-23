39636a40b593de9cccc6709ec86365c1641e97cc
Thorsten Reisinger <52202041@edu.campus02.at>
2022-09-23 17:06:47 +0200

    My First Class created; args as input parameter written to console

---
+++ MyFirstClass.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+    //wir haben unsere erste Klasse erzeugt
+
+    //main --> TAB
+    public static void main(String[] args) {
+        //erste Ausgabe
+        //sout --> TAB
+        System.out.println("Ich werde auf der Konsole angezeigt");
+
+        System.out.println(Arrays.toString(args));
+    }
+}
