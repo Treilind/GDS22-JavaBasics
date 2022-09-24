f5d815d32fedaa32b5269e76f31f70374698b147
Thorsten Reisinger <52202041@edu.campus02.at>
2022-09-24 12:03:42 +0200

    Pre- und Post Inkrement

--- ArethmetischeOperationen.java
+++ ArethmetischeOperationen.java
@@ -23,5 +23,18 @@ public class ArethmetischeOperationen {
         System.out.println("Dividieren: " + div);
         System.out.println("Dividieren: " + divid);
         System.out.println("Modulo: " + (a % b));
+
+
+        //a++ beudeutet a = a + 1
+        System.out.println("a++");
+        //ich erwate mir 32
+        System.out.println(a++);
+        System.out.println("a = " + a); //ich erwate mir 33
+
+        //++b
+        System.out.println("++b");
+        //11
+        System.out.println(++b);
+        System.out.println("b = " + b); //11
     }
 }
